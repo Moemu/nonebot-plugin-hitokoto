9a31aeb21a700263ec1f5c06104e0c9266ef85c1
Moemu <WhitemuTeam@outlook.com>
2025-04-20 01:59:51 +0800

    ✨ 引入可配置项

--- nonebot_plugin_hitokoto/__init__.py
+++ nonebot_plugin_hitokoto/__init__.py
@@ -1,20 +1,42 @@
 import httpx
-from nonebot import logger, on_command
-from nonebot.adapters import Message
-from nonebot.matcher import Matcher
-from nonebot.params import CommandArg
 
-hitokoto_matcher = on_command("一言", aliases={"一句"})
+from nonebot import logger, get_plugin_config
+from nonebot.plugin import on_command
+from pydantic import BaseModel
+
+class ScopeConfig(BaseModel):
+    c: list[str] = []
+    """句子类型"""
+    base_url: str = "https://international.v1.hitokoto.cn"
+    """请求地址"""
+    priority: int = 10
+    """指令优先级"""
+
+class Config(BaseModel):
+    hitokoto: ScopeConfig = ScopeConfig()
+
+plugin_config = get_plugin_config(Config).hitokoto
+
+
+hitokoto_matcher = on_command("一言", aliases={"一句"}, block=True, priority=plugin_config.priority)
+
+def generate_url() -> str:
+    """构建请求url"""
+    data = f"{plugin_config.base_url}?"
+    for c in plugin_config.c:
+        data += f"c={c}&"
+    return data.strip("&")
 
 @hitokoto_matcher.handle()
-async def hitokoto(matcher: Matcher, args: Message = CommandArg()):
-    if args:
-        return
+async def hitokoto():
     async with httpx.AsyncClient() as client:
-        response = await client.get("https://v1.hitokoto.cn?c=a&c=b&c=c&c=d&c=h")
+        response = await client.get(generate_url())
+
     if response.is_error:
-        logger.error("获取一言失败")
-        return
+        error_msg = "获取一言失败"
+        logger.error(error_msg)
+        return error_msg
+    
     data = response.json()
     msg = data["hitokoto"]
     add = ""
@@ -24,4 +46,5 @@ async def hitokoto(matcher: Matcher, args: Message = CommandArg()):
         add += f"{from_who}"
     if add:
         msg += f"\n——{add}"
-    await matcher.finish(msg)
+
+    await hitokoto_matcher.finish(msg)
